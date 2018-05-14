32c01ccff021db6aca04a2485b8c15b285e1de71
Marcelo dos Santos <mdssjc@gmail.com>
2018-05-14 18:27:49 -0300

    Preliminar dos atalhos.

--- lang/racket/racket.el
+++ lang/racket/racket.el
@@ -54,37 +54,35 @@
 
   (map! :map racket-mode-map
         :localleader
-        :n "C-c C-c"     #'racket-run
-        :n "C-c C-k"     #'racket-run
-        :n "C-c C-z"     #'racket-repl
-        :n "<f5>"        #'racket-run-and-switch-to-repl
-        :n "M-C-<f5>"    #'racket-racket
-        :n "C-<f5>"      #'racket-test
-        :n "C-c C-t"     #'racket-test
-        :n "C-c C-l"     #'racket-logger
-        :n "C-c C-o"     #'racket-profile
-        :n "M-C-x"       #'racket-send-definition
-        :n "C-x C-e"     #'racket-send-last-sexp
-        :n "C-c C-r"     #'racket-send-region
-        :n "C-c C-e x"   #'racket-expand-definition
-        :n "C-c C-e e"   #'racket-expand-last-sexp
-        :n "C-c C-e r"   #'racket-expand-region
-        :n "C-c C-e a"   #'racket-expand-again
-        :n "C-c C-x C-f" #'racket-open-require-path
-        :n "TAB"         #'indent-for-tab-command
-        :n "M-C-u"       #'racket-backward-up-list
-        :n "["           #'racket-smart-open-bracket
-        :n ")"           #'racket-insert-closing
-        :n "]"           #'racket-insert-closing
-        :n "}"           #'racket-insert-closing
-        :n "C-c C-p"     #'racket-cycle-paren-shapes
-        :n "M-C-y"       #'racket-insert-lambda
-        :n "C-c C-d"     #'racket-doc
-        :n "C-c C-."     #'racket-describe
-        :n "M-."         #'racket-visit-definition
-        :n "M-C-."       #'racket-visit-module
-        :n "M-,"         #'racket-unvisit
-        :n "C-c C-f"     #'racket-fold-all-tests
-        :n "C-c C-u"     #'racket-unfold-all-tests
-        :n "a"           #'racket-align
-        :n "A"           #'racket-unalign))
+        :n "c"   #'racket-run
+        :n "z"   #'racket-repl
+        :n "r"   #'racket-run-and-switch-to-repl
+        :n "R"   #'racket-racket
+        :n "t"   #'racket-test
+        :n "l"   #'racket-logger
+        :n "o"   #'racket-profile
+        :n "s d" #'racket-send-definition
+        :n "s l" #'racket-send-last-sexp
+        :n "s r" #'racket-send-region
+        :n "e d" #'racket-expand-definition
+        :n "e l" #'racket-expand-last-sexp
+        :n "e r" #'racket-expand-region
+        :n "e a" #'racket-expand-again
+        :n "P"   #'racket-open-require-path
+        :n "TAB" #'indent-for-tab-command
+        :n "u"   #'racket-backward-up-list
+        :n "["   #'racket-smart-open-bracket
+        :n ")"   #'racket-insert-closing
+        :n "]"   #'racket-insert-closing
+        :n "}"   #'racket-insert-closing
+        :n "p"   #'racket-cycle-paren-shapes
+        :n "y"   #'racket-insert-lambda
+        :n "d"   #'racket-doc
+        :n "."   #'racket-describe
+        :n "M-." #'racket-visit-definition
+        :n "C-." #'racket-visit-module
+        :n ","   #'racket-unvisit
+        :n "f"   #'racket-fold-all-tests
+        :n "F"   #'racket-unfold-all-tests
+        :n "a"   #'racket-align
+        :n "A"   #'racket-unalign))
