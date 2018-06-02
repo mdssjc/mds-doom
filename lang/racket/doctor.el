1a33451932e05f0fc9af4e27504a00af621b55c6
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-02 18:49:59 -0300

    Melhorias nas mensagens de aviso.

--- lang/racket/doctor.el
+++ lang/racket/doctor.el
@@ -1,13 +1,11 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; -*-
 ;;; lang/racket/doctor.el
 
-;; FIXME: corrigir as mensagens...
-
 (unless (executable-find "drracket")
-  (warn! "Could not find drracket executable; code-completion, syntax checking and jump-to-definition functionality will be disabled."))
+  (warn! "Could not find drracket executable; code-completion and syntax checking will not work."))
 
 (unless (executable-find "racket")
-  (warn! "Could not find racket executable; build commands will be disabled."))
+  (warn! "Could not find racket executable; REPL will not work."))
 
 (unless (executable-find "raco")
-  (warn! "Could not find raco executable; build commands will be disabled."))
+  (warn! "Could not find raco executable; commands for install packages and build libraries will not work."))
