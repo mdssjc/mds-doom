6664a444d7d8d2780e3b9c78a0cba5db6964b1ac
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-03 14:33:17 -0300

    Atalhos no menubar.

--- config.el
+++ config.el
@@ -190,6 +190,21 @@
     ("Insert Macro"       :exec kmacro-insert-counter))
   :prompt "Option: ")
 
+(easy-menu-define kmacro-menu nil
+  "Keyboard Macro Menu."
+  '("Keyboard Macro"
+    ["Universal Argument" universal-argument        :help "Begin a numeric argument for the following command."]
+    "--"
+    ["Start Macro"        kmacro-start-macro        :help "Record subsequent keyboard input, defining a keyboard macro."]
+    ["End Macro"          kmacro-end-macro          :help "Finish defining a keyboard macro."]
+    ["End And Call Macro" kmacro-end-and-call-macro :help "Call last keyboard macro, ending it first if currently being defined."]
+    "--"
+    ["Kmacro Bind To Key" kmacro-bind-to-key        :help "When not defining or executing a macro, offer to bind last macro to a key."]
+    ["Name Last Macro"    kmacro-name-last-macro    :help "Assign a name to the last keyboard macro defined."]
+    ["Insert Macro"       insert-kbd-macro          :help "Insert in buffer the definition of kbd macro MACRONAME, as Lisp code."]))
+
+(easy-menu-add-item menu-bar-tools-menu nil kmacro-menu "grep")
+
 (add-hook! 'rg-mode-hook         'wgrep-setup)
 (add-hook! 'emacs-lisp-mode-hook 'lispy-mode)
 (add-hook! 'racket-mode-hook     'lispy-mode)
