955ee6b22d0743a262e4030b619980d3bbaba47a
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-17 12:36:38 -0300

    Refatoração nas configurações.

--- config.el
+++ config.el
@@ -8,16 +8,6 @@
 (def-package! restart-emacs
   :commands restart-emacs)
 
-(def-package! ivy
-  :config
-  (setq ivy-count-format "(%d/%d) "))
-
-(def-package! ivy-rich
-  :after ivy
-  :config
-  (setq ivy-use-virtual-buffers t
-        ivy-rich-path-style 'abbrev))
-
 (def-package! avy
   :commands (avy-goto-char-2
              avy-goto-line
@@ -86,51 +76,6 @@
   :config
   (cakecrumbs-auto-setup))
 
-;; ---
-
-
-
-;;
-;; Config
-;;
-
-;; General
-(load! "ui/modeline")
-(load! "+bindings")
-(load! "+evil-commands")
-
-;; Syntatic
-(add-hook! company-mode
-  (setq company-idle-delay 0
-        company-minimum-prefix-length 1
-        company-show-numbers t
-        company-selection-wrap-around t
-        prescient-save-file (concat doom-cache-dir "prescient-save.el"))
-  (ivy-prescient-mode)
-  (company-prescient-mode)
-  (prescient-persist-mode))
-
-(def-package! ispell
-  ;; :commands (company-ispell)
-  :config
-  (cond ((executable-find "hunspell")
-         (setq ispell-program-name "hunspell"
-               ispell-really-hunspell t))
-        ((executable-find "aspell")
-         (setq ispell-program-name "aspell"
-               ispell-really-aspell t
-               ispell-extra-args '("--sug-mode=ultra"))))
-  (setq ispell-list-command "--list"
-        ispell-look-p nil
-        ispell-grep-command "rg"
-        ispell-grep-options "-i"
-        ispell-dictionary   "pt_BR"
-        ispell-choices-win-default-height 5
-        ispell-lazy-highlight nil)
-  (ispell-pt-br))
-;; ---
-
-;; Semantic
 (def-package! google-this
   :commands (google-this-mode
              google-this-search
@@ -157,103 +102,67 @@
   :config
   (setq google-translate-show-phonetic t))
 
-;; FIXME - ver o install do helm
-(def-package! helm-dash
-  :config
-  (setq helm-dash-docsets-path "~/.local/share/Zeal/Zeal/docsets"))
+(def-package! clojure-snippets
+  :after clojure-mode)
+
 ;; ---
 
-(def-menu! +macros
-  "Keyboard Macro Menu."
-  '(("Universal Argument" :exec universal-argument)
-    ("Start Macro"        :exec kmacro-start-macro)
-    ("End Macro"          :exec kmacro-end-macro)
-    ("End And Call Macro" :exec kmacro-end-and-call-macro)
-    ("Kmacro Bind To Key" :exec kmacro-bind-to-key)
-    ("Name Last Macro"    :exec kmacro-name-last-macro)
-    ("Insert Macro"       :exec kmacro-insert-counter))
-  :prompt "Option: ")
 
-(easy-menu-define kmacro-menu nil
-  "Keyboard Macro Menu."
-  '("Keyboard Macro"
-    ["Universal Argument" universal-argument        :help "Begin a numeric argument for the following command."]
-    "--"
-    ["Start Macro"        kmacro-start-macro        :help "Record subsequent keyboard input, defining a keyboard macro."]
-    ["End Macro"          kmacro-end-macro          :help "Finish defining a keyboard macro."]
-    ["End And Call Macro" kmacro-end-and-call-macro :help "Call last keyboard macro, ending it first if currently being defined."]
-    "--"
-    ["Kmacro Bind To Key" kmacro-bind-to-key        :help "When not defining or executing a macro, offer to bind last macro to a key."]
-    ["Name Last Macro"    kmacro-name-last-macro    :help "Assign a name to the last keyboard macro defined."]
-    ["Insert Macro"       insert-kbd-macro          :help "Insert in buffer the definition of kbd macro MACRONAME, as Lisp code."]))
 
-(easy-menu-add-item menu-bar-tools-menu nil kmacro-menu "grep")
+;;
+;; Config
+;;
 
-(add-hook 'rg-mode-hook         #'wgrep-setup)
-(add-hook 'emacs-lisp-mode-hook #'lispy-mode)
-(add-hook 'racket-mode-hook     #'lispy-mode)
-(add-hook 'lispy-mode-hook      #'lispyville-mode)
+(load! "ui/modeline")
+(load! "+bindings")
+(load! "+evil-commands")
+(load! "+menus")
+
+
+(add-hook 'rg-mode-hook          #'wgrep-setup)
+(add-hook 'lispy-mode-hook       #'lispyville-mode)
+(add-hook 'clojure-mode-hook     #'lispy-mode)
+(add-hook 'racket-mode-hook      #'lispy-mode)
+(add-hook 'emacs-lisp-mode-hook  #'lispy-mode)
+(remove-hook 'markdown-mode-hook #'auto-fill-mode)
+
+
+(after! company
+  (setq company-idle-delay 0
+        company-minimum-prefix-length 1
+        company-show-numbers t
+        company-selection-wrap-around t
+        prescient-save-file (concat doom-cache-dir "prescient-save.el"))
+  (ivy-prescient-mode)
+  (company-prescient-mode)
+  (prescient-persist-mode))
+
+(after! ivy
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-use-virtual-buffers t
+        ivy-rich-path-style 'abbrev)
+  (setq helm-dash-docsets-path "~/.local/share/Zeal/Zeal/docsets"))
+
+(after! ispell
+  (cond ((executable-find "hunspell")
+         (setq ispell-program-name "hunspell"
+               ispell-really-hunspell t))
+        ((executable-find "aspell")
+         (setq ispell-program-name "aspell"
+               ispell-really-aspell t
+               ispell-extra-args '("--sug-mode=ultra"))))
+  (setq ispell-list-command "--list"
+        ispell-look-p nil
+        ispell-grep-command "rg"
+        ispell-grep-options "-i"
+        ispell-dictionary   "pt_BR"
+        ispell-choices-win-default-height 5
+        ispell-lazy-highlight nil)
+  (ispell-pt-br))
 
 (after! magit
   (setq magit-diff-refine-hunk 'all))
 
-(after! org
-  (progn
-    (setq org-todo-keywords '((sequence "TODO(t)" "DOING(d)" "BLOCKED(b)" "REVIEW(r)"
-                                        "|" "DONE(e)" "ARCHIVED(a)" "CANCELED(c)"))
-          org-todo-keyword-faces '(("TODO"     . org-warning)
-                                   ("DOING"    . "yellow")
-                                   ("BLOCKED"  . "red")
-                                   ("REVIEW"   . "orange")
-                                   ("DONE"     . "green")
-                                   ("ARCHIVED" . "blue")
-                                   ("CANCELED" . "red1"))
-          org-pretty-entities t
-          org-hide-emphasis-markers t
-          org-tags-column -77
-          ;; Agenda
-          org-agenda-files '("~/Documents/GTD/someday-maybe.org"
-                             "~/Documents/GTD/calendar.org"
-                             "~/Documents/GTD/projects.org")
-          org-agenda-custom-commands '(("W" "Weekly Review"
-                                        ((agenda "" ((org-agenda-ndays 7)))
-                                         (stuck "")
-                                         (todo "DOING")
-                                         (org-agenda-entry-types '(:deadline))
-                                         (org-deadline-warning-days 30))))
-          ;; Refile
-          org-refile-use-outline-path 'file
-          org-outline-path-complete-in-steps nil
-          org-refile-allow-creating-parent-nodes 'confirm
-          completing-read-function 'ivy-completing-read
-          org-refile-targets '(("~/Documents/GTD/trash.org"         :maxlevel . 1)
-                               ("~/Documents/GTD/someday-maybe.org" :maxlevel . 1)
-                               ("~/Documents/GTD/reference.org"     :maxlevel . 1)
-                               ("~/Documents/GTD/projects.org"      :maxlevel . 3)
-                               ("~/Documents/GTD/next-action.org"   :maxlevel . 2)
-                               ("~/Documents/GTD/waiting-for.org"   :maxlevel . 1))
-          ;; Templates - Capture
-          org-capture-templates '(("n" "Notes" entry
-                                   (file+headline +org-default-notes-file "Notes")
-                                   "* %i%?"
-                                   :prepend t :kill-buffer t)
-                                  ("t" "Inbox" entry
-                                   (file+headline "~/Documents/GTD/inbox.org" "Things")
-                                   "* %i%?\nCreate in %U"
-                                   :prepend t :kill-buffer t)))
-    ;; Encrypt all entries before saving
-    ;; (org-crypt-use-before-save-magic)
-    ;; (setq org-tags-exclude-from-inheritance '("crypt"))
-    ;; (let ((secrets-path "~/.config/doom/modules/private/mdssjc/secrets/secrets.el"))
-    ;;   (if (file-exists-p secrets-path)
-    ;;       (progn (load secrets-path)
-    ;;              ;; GPG key to use for encryption
-    ;;              (setq org-crypt-key user-password))
-    ;;     (message "Use mds-secrets-template.el as the basis for the secrets.el file.")))
-    ))
-
-;; Clojure
-(add-hook 'clojure-mode-hook #'lispy-mode)
 (after! cider
   (setq nrepl-log-messages t
         cider-repl-display-in-current-window t
@@ -274,10 +183,56 @@
                                         company-dabbrev
                                         company-files)))
 
-(def-package! clojure-snippets
-  :after clojure-mode)
-;; ---
-
-;; Markdown
-(remove-hook 'markdown-mode-hook #'auto-fill-mode)
-;; ---
+(after! org
+  (setq org-todo-keywords '((sequence "TODO(t)" "DOING(d)" "BLOCKED(b)" "REVIEW(r)"
+                                      "|" "DONE(e)" "ARCHIVED(a)" "CANCELED(c)"))
+        org-todo-keyword-faces '(("TODO"     . org-warning)
+                                 ("DOING"    . "yellow")
+                                 ("BLOCKED"  . "red")
+                                 ("REVIEW"   . "orange")
+                                 ("DONE"     . "green")
+                                 ("ARCHIVED" . "blue")
+                                 ("CANCELED" . "red1"))
+        org-pretty-entities t
+        org-hide-emphasis-markers t
+        org-tags-column -77
+        ;; Agenda
+        org-agenda-files '("~/Documents/GTD/someday-maybe.org"
+                           "~/Documents/GTD/calendar.org"
+                           "~/Documents/GTD/projects.org")
+        org-agenda-custom-commands '(("W" "Weekly Review"
+                                      ((agenda "" ((org-agenda-ndays 7)))
+                                       (stuck "")
+                                       (todo "DOING")
+                                       (org-agenda-entry-types '(:deadline))
+                                       (org-deadline-warning-days 30))))
+        ;; Refile
+        org-refile-use-outline-path 'file
+        org-outline-path-complete-in-steps nil
+        org-refile-allow-creating-parent-nodes 'confirm
+        completing-read-function 'ivy-completing-read
+        org-refile-targets '(("~/Documents/GTD/trash.org"         :maxlevel . 1)
+                             ("~/Documents/GTD/someday-maybe.org" :maxlevel . 1)
+                             ("~/Documents/GTD/reference.org"     :maxlevel . 1)
+                             ("~/Documents/GTD/projects.org"      :maxlevel . 3)
+                             ("~/Documents/GTD/next-action.org"   :maxlevel . 2)
+                             ("~/Documents/GTD/waiting-for.org"   :maxlevel . 1))
+        ;; Templates - Capture
+        org-capture-templates '(("n" "Notes" entry
+                                 (file+headline +org-default-notes-file "Notes")
+                                 "* %i%?"
+                                 :prepend t :kill-buffer t)
+                                ("t" "Inbox" entry
+                                 (file+headline "~/Documents/GTD/inbox.org" "Things")
+                                 "* %i%?\nCreate in %U"
+                                 :prepend t :kill-buffer t)))
+  ;; Encrypt all entries before saving
+  ;; (org-crypt-use-before-save-magic)
+  ;; (setq org-tags-exclude-from-inheritance '("crypt"))
+  ;; (let ((secrets-path "~/.config/doom/modules/private/mdssjc/secrets/secrets.el"))
+  ;;   (if (file-exists-p secrets-path)
+  ;;       (progn (load secrets-path)
+  ;;              ;; GPG key to use for encryption
+  ;;              (setq org-crypt-key user-password))
+  ;;     (message "Use mds-secrets-template.el as the basis for the secrets.el file.")))
+  )
